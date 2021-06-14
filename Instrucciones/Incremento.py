7c85e74caaf0ceb05f2996c8fa4459f6ef482b8e
Carlos Castro <2786954930101@ingenieria.usac.edu.gt>
2021-06-13 22:13:08 -0600

    incremento y decremento

---
+++ Instrucciones/Incremento.py
@@ -0,0 +1,25 @@
+from TS.Tipo import TIPO
+from TS.Exception import Excepcion
+from Abstract.instruccion import Instruccion
+from TS.Simbolo import Simbolo
+
+
+class Incremento(Instruccion):
+    def __init__(self, identificador, fila, columna):
+        self.identificador = identificador
+        self.tipo = None
+        self.fila = fila
+        self.columna = columna
+
+    def interpretar(self, tree, table):
+        symbol = table.getTabla(self.identificador)
+        if symbol != None:
+            if symbol.tipo == TIPO.ENTERO or symbol.tipo == TIPO.DECIMAL:
+                self.tipo = symbol.tipo
+                updateSymbol = Simbolo(symbol.id,symbol.tipo,symbol.fila, symbol.columna,symbol.valor+1)
+                table.actualizarTabla(updateSymbol)
+            else:
+                return Excepcion("Semantico", "Tipo de dato no se puede incrementar.", self.fila, self.columna)
+        else:
+           return Excepcion("Semantico", "Variable " + self.identificador + " no encontrada", self.fila, self.columna)
+        return symbol.valor
