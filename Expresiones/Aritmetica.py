64fac104c1ba135469372c42c1995b0b44942bef
Carlos Castro <2786954930101@ingenieria.usac.edu.gt>
2021-06-20 19:30:04 -0600

    mod by zero fixed

--- Expresiones/Aritmetica.py
+++ Expresiones/Aritmetica.py
@@ -166,6 +166,8 @@ class Aritmetica(Instruccion):
                 return pow(self.obtenerVal(self.OperacionIzq.tipo, izq), self.obtenerVal(self.OperacionDer.tipo, der))
             return Excepcion("Semantico", "Tipo Erroneo de operacion para (**).", self.fila, self.columna)
         elif self.operador == OperadorAritmetico.MOD:  # -------------------------- MODULO
+            if der == 0:
+                return Excepcion("Semantico", "Modulo por 0.", self.fila, self.columna)
             # ---------- entero
             if self.OperacionIzq.tipo == TIPO.ENTERO and self.OperacionDer.tipo == TIPO.ENTERO:
                 self.tipo = TIPO.DECIMAL
